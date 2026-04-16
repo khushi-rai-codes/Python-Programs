1b2e1e3825b436290be9dcaf7b3e8170388b31f2
khushi-rai-codes <kr14052006@gmail.com>
2026-04-16 21:07:47 +0530

    Create avg_marks_calculator.py
    A simple Python program that calculates the average of three subject marks using user input and arithmetic operations.

---
+++ avg_marks_calculator.py
@@ -0,0 +1,7 @@
+marks1 = int(input("Enter marks of subject 1: "))
+marks2 = int(input("Enter marks of subject 2: "))
+marks3 = int(input("Enter marks of subject 3: "))
+
+average = (marks1 + marks2 + marks3) / 3
+
+print("Average Marks =", average)
