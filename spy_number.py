ae1f82b17bc885f9c9da81b2f785175c1cc72a38
khushi-rai-codes <kr14052006@gmail.com>
2026-05-23 19:52:49 +0530

    Create spy_number.py

---
+++ spy_number.py
@@ -0,0 +1,13 @@
+num = int(input("Enter a number: "))
+temp = num
+sum_digits = 0
+product_digits = 1
+while temp > 0:
+    digit = temp % 10
+    sum_digits += digit
+    product_digits *= digit
+    temp //= 10
+if sum_digits == product_digits:
+    print(num, "is a Spy Number")
+else:
+    print(num, "is not a Spy Number")
