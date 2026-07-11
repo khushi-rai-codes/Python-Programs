6cf6b772f67a20c39295509841a8d780ba3e0f6a
Khushi <kr14052006@gmail.com>
2026-07-11 21:23:31 +0530

    Create word_frequency_counter.py

---
+++ word_frequency_counter.py
@@ -0,0 +1,8 @@
+sentence = input("Enter a sentence: ")
+words = sentence.lower().split()
+frequency = {}
+for word in words:
+    frequency[word] = frequency.get(word, 0) + 1
+print("\nWord Frequency:")
+for word, count in frequency.items():
+    print(f"{word} : {count}")
